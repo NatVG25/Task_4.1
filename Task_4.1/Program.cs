d3e4de9f774db5814d0f5fc0a01b7d4585e4cdc4
79827 <79827@DESKTOP-0UET2M8>
2021-11-21 21:43:55 +0500

    Добавьте файлы проекта.

---
+++ Task_4.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите любое положительное целое число"); //Ввод исходного числа
+            int n = Convert.ToInt32(Console.ReadLine());
+            int Sum = 0; //инициализвция переменной Sum
+            for (int i = 1; i <= n; i++) //задание цикла с параметром
+            {
+                int q = 2 * i - 1;
+                Console.WriteLine("Квадрат числа {0} равен {1}", i, Sum += q); //вывод результата
+            }
+            Console.ReadKey();
+        }
+    }
+}
